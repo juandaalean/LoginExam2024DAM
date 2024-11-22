ca0ae7db5212e3c2f8bbb17d4ff52135def273ab
Chema <josem.perram@educa.jcyl.es>
2024-11-22 02:11:34 +0100

    Start project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,23 +15,17 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
-        //testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
